2f7d22cc0dfa80596876dab0c390771594538d18
Lionel COTE <grcote7@gmail.com>
2023-05-26 11:13:26 +0200

    Feat: VueJS with TypeScript

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,23 @@
+<template>
+	<h1 v-text="state.title"></h1>
+	<h2 v-text="state.subTitle"></h2>
+
+	<button @click="editTitle('Hello world Edited')">Éditer title</button>
+	<button @click="editSubTitle('80')">Éditer subTitle</button>
+</template>
+
+<script setup lang="ts">
+import { reactive } from "vue";
+
+const state = reactive({
+	title: "Hello World!" as string,
+	subTitle: 90 as string | number,
+});
+
+const editTitle = (editTitle: string) => {
+	state.title = editTitle;
+};
+const editSubTitle = (editSubTitle: string | number) => {
+	state.subTitle = editSubTitle;
+};
+</script>
