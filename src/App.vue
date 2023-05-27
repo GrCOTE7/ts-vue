c076809493abb54c5af676ae3f4b1f5a55cd188c
Lionel COTE <grcote7@gmail.com>
2023-05-27 14:33:40 +0200

    Feat: Reusable dynamic component

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,25 @@
 <template>
-	<HelloWorld />
+  <div class="mx-auto container">
+    <select-options
+        :options="people"
+
+        v-model="userId"
+      />
+  </div>
 </template>
 
 <script setup lang="ts">
-import HelloWorld from "./components/HelloWorld.vue";
+import SelectOptions from "./components/SelectOptions.vue";
+import {ref} from 'vue';
+
+const userId = ref<number>(5);
+
+const people = [
+  { id: 1, name: 'Wade Cooper' },
+  { id: 2, name: 'Arlene Mccoy' },
+  { id: 3, name: 'Devon Webb' },
+  { id: 4, name: 'Tom Cook' },
+  { id: 5, name: 'Tanya Fox' },
+  { id: 6, name: 'Hellen Schmidt' },
+]
 </script>
